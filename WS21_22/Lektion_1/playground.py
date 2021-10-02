e485849e2688c426077b60b15f7d0f15a35c4044
Hana <hana.zupan@hotmail.com>
2021-10-02 11:00:47 +0200

    Making sure code in handout actually works.

---
+++ WS21_22/Lektion_1/playground.py
@@ -0,0 +1,25 @@
+print("Hallo Welt!")
+name = "Hana"
+alter = 23
+print("Mein Name ist " + name + " und ich bin " + str(alter) + " Jahre alt.")
+print("Mein Name ist", name, "und ich bin", alter, "Jahre alt.")
+print(f"Mein Name ist {name} und ich bin {alter} Jahre alt.")
+print(round(1.625))
+print(int("13"))
+for natuerliche_zahl in range(15):
+    print("Hallo Welt!")
+    print(f"Ich drucke jetzt die Zahl {natuerliche_zahl}!")
+beste_liste = [3, 2.5, "Hallo"]
+print(beste_liste)
+beste_liste.append(113)
+print(beste_liste)
+beste_liste.remove(2.5)
+print(beste_liste)
+beste_liste.reverse()
+print(beste_liste)
+print(sum([5, 2.3, 8]))
+print(len(beste_liste))
+mein_name = "hana"
+print(mein_name.capitalize())
+print(mein_name.upper())
+print(mein_name.center(8, "!"))
