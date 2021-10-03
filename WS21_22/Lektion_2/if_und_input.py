88f19072196a3ebc04183c10e4970e523e0b44bf
Hana <hana.zupan@hotmail.com>
2021-10-03 13:44:28 +0200

    Finished the if-sentences and input part.

---
+++ WS21_22/Lektion_2/if_und_input.py
@@ -0,0 +1,32 @@
+import turtle
+
+# lass den Benutzer wählen, welche Farbe gut aussieht!
+farbe = input("Welche farbe? ")
+turtle.color(farbe)
+for i in range(40):
+    turtle.forward(15-i/3)
+    turtle.left(i*2+i/9)
+turtle.hideturtle()
+
+turtle.clearscreen()
+
+# einfache if-Sätze: eine Entscheidung treffen
+kosten = int(input("Wie viel kosten die Ferien? "))
+freunde = int(input("Wie viele Menschen werden teilnehmen? "))
+deine_kosten = kosten/freunde
+print(f"Du müsstest im diesen Fall {round(deine_kosten)}€ bezahlen.")
+if deine_kosten > 100:
+    print("Das kannst du dich leider nicht leisten.")
+else:
+    print("Das geht!")
+
+# if-Sätze mit turtle
+for schritt in range(10):
+    turtle.forward(50)
+    richtung = input("In welche Richtung willst du abbiegen? (l/r) ")
+    if richtung == "r":
+        turtle.right(90)
+    else:
+        turtle.left(90)
+
+turtle.done()
