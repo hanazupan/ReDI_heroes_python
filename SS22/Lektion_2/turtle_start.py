7bf21bdeeefaf2eac59494b612ec4c9c707fecd5
Hana <hana.zupan@hotmail.com>
2022-03-21 12:02:02 +0100

    Intro turtle

---
+++ SS22/Lektion_2/turtle_start.py
@@ -0,0 +1,49 @@
+# eine Bibliothek bzw ein Paket
+import turtle as t
+
+# Beispiel
+t.width(5)
+t.speed(1)
+t.turtlesize(2)
+t.forward(60)
+t.left(45)
+t.forward(60)
+t.right(40)
+t.backward(70)
+t.circle(20)
+
+# einfache Befehle - Farbe, Richtung, Geschwindigkeit
+# Hinweis: forward() = fd(), backward() = bk(), right() = rt(), left() = lt()
+t.color("Blue")
+t.speed(10)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.width(10)
+t.color("Red")
+t.circle(30)
+t.fd(50)
+t.rt(90)
+t.bk(50)
+
+
+t.hideturtle()
+
+#turtle.clearscreen()
+
+# Zeichen H
+t.color("Green")
+t.width(5)
+t.speed(10)
+t.left(90)
+t.forward(200)
+t.backward(100)
+t.right(90)
+t.forward(70)
+t.right(90)
+t.forward(100)
+t.backward(200)
+
+t.hideturtle()
+
+#turtle.done()
